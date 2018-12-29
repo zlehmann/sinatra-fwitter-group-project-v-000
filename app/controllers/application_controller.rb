64c09aee00d2af78636852f3c37e5043d4db4e7a
Zachary Lehmann <zlehmann@gmail.com>
2018-12-29 20:31:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,74 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :homepage
+  end
+
+  get '/signup' do
+    @is_logged_in = Helpers.is_logged_in?(session)
+    if @is_logged_in != true
+      erb :signup
+    else
+      @user = Helpers.current_user(session)
+      redirect to "/tweets"
+    end
+  end
+
+  post '/signup' do
+    if params['username'] == "" || params['email'] == "" || params['password'] == ""
+      redirect to '/signup'
+    else
+      @session = session
+      @new_user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+      session[:user_id] = @new_user.id
+      redirect to "/tweets"
+    end
+  end
+
+  get '/login' do
+    @is_logged_in = Helpers.is_logged_in?(session)
+    if @is_logged_in != true
+      erb :login
+    else
+      @user = Helpers.current_user(session)
+      redirect to "/tweets"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params['username'], password: params['password'])
+    if @user == nil
+      redirect to '/login'
+    else
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/tweets' do
+    @is_logged_in = Helpers.is_logged_in?(session)
+    if @is_logged_in != true
+      redirect to '/login'
+    else
+      @user = Helpers.current_user(session)
+      erb :tweets
+    end
+  end
+
+  get '/logout' do
+    @is_logged_in = Helpers.is_logged_in?(session)
+    if @is_logged_in != true
+      redirect to '/login'
+    else
+      session.clear
+      redirect to "/login"
+    end
+  end
 end
