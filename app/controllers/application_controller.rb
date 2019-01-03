e1042c321cb12b22d470dbbdefdc90940289a42d
Zachary Lehmann <zlehmann@gmail.com>
2019-01-03 02:12:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params['username'])
-    
+
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to '/tweets'
